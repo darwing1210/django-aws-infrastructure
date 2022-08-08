b5b1cdfc9ded29b28a9f0fa1d74fc97cf69b0dd3
Yevhen Bondar <eugenij.bondar@gmail.com>
2022-08-08 12:02:27 +0300

    add worker and beat ECS

--- ecs.tf
+++ ecs.tf
@@ -17,6 +17,10 @@ locals {
 
     domain = var.prod_backend_domain
     secret_key = var.prod_backend_secret_key
+
+    sqs_access_key = aws_iam_access_key.prod_sqs.id
+    sqs_secret_key = aws_iam_access_key.prod_sqs.secret
+    sqs_name = aws_sqs_queue.prod.name
   }
 }
 
@@ -166,6 +170,16 @@ resource "aws_cloudwatch_log_stream" "prod_backend_migrations" {
   log_group_name = aws_cloudwatch_log_group.prod_backend.name
 }
 
+resource "aws_cloudwatch_log_stream" "prod_backend_worker" {
+  name           = "prod-backend-worker"
+  log_group_name = aws_cloudwatch_log_group.prod_backend.name
+}
+
+resource "aws_cloudwatch_log_stream" "prod_backend_beat" {
+  name           = "prod-backend-beat"
+  log_group_name = aws_cloudwatch_log_group.prod_backend.name
+}
+
 # Migrations
 
 resource "aws_ecs_task_definition" "prod_backend_migration" {
@@ -189,5 +203,91 @@ resource "aws_ecs_task_definition" "prod_backend_migration" {
   depends_on         = [aws_db_instance.prod]
   execution_role_arn = aws_iam_role.ecs_task_execution.arn
   task_role_arn      = aws_iam_role.prod_backend_task.arn
+}
+
+# Worker
+
+resource "aws_ecs_task_definition" "prod_backend_worker" {
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+
+  family = "backend-worker"
+  container_definitions = templatefile(
+    "templates/backend_container.json.tpl",
+    merge(
+      local.container_vars,
+      {
+        name       = "prod-backend-worker"
+        command    = ["celery", "-A", "django_aws", "worker", "--loglevel", "info"]
+        log_stream = aws_cloudwatch_log_stream.prod_backend_worker.name
+      },
+    )
+  )
+  depends_on = [aws_sqs_queue.prod, aws_db_instance.prod]
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+  task_role_arn      = aws_iam_role.prod_backend_task.arn
+}
+
+resource "aws_ecs_service" "prod_backend_worker" {
+  name                               = "prod-backend-worker"
+  cluster                            = aws_ecs_cluster.prod.id
+  task_definition                    = aws_ecs_task_definition.prod_backend_worker.arn
+  desired_count                      = 2
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+  launch_type                        = "FARGATE"
+  scheduling_strategy                = "REPLICA"
+  enable_execute_command             = true
+
+  network_configuration {
+    security_groups  = [aws_security_group.prod_ecs_backend.id]
+    subnets          = [aws_subnet.prod_private_1.id, aws_subnet.prod_private_2.id]
+    assign_public_ip = false
+  }
+}
+
+# Beat
+
+resource "aws_ecs_task_definition" "prod_backend_beat" {
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+
+  family = "backend-beat"
+  container_definitions = templatefile(
+    "templates/backend_container.json.tpl",
+    merge(
+      local.container_vars,
+      {
+        name       = "prod-backend-beat"
+        command    = ["celery", "-A", "django_aws", "beat", "--loglevel", "info"]
+        log_stream = aws_cloudwatch_log_stream.prod_backend_beat.name
+      },
+    )
+  )
+  depends_on = [aws_sqs_queue.prod, aws_db_instance.prod]
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+  task_role_arn      = aws_iam_role.prod_backend_task.arn
+}
+
+resource "aws_ecs_service" "prod_backend_beat" {
+  name                               = "prod-backend-beat"
+  cluster                            = aws_ecs_cluster.prod.id
+  task_definition                    = aws_ecs_task_definition.prod_backend_beat.arn
+  desired_count                      = 1
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+  launch_type                        = "FARGATE"
+  scheduling_strategy                = "REPLICA"
+  enable_execute_command             = true
+
+  network_configuration {
+    security_groups  = [aws_security_group.prod_ecs_backend.id]
+    subnets          = [aws_subnet.prod_private_1.id, aws_subnet.prod_private_2.id]
+    assign_public_ip = false
+  }
 }
 
